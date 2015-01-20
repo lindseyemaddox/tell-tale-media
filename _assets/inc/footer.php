f9529927f77f5af16460e029ec404f3e7e13aaa8
lindseyemaddox <lindseyemaddox@gmail.com>
2015-01-19 21:39:29 -0600

    initial setup

---
+++ _assets/inc/footer.php
@@ -0,0 +1,14 @@
+
+		<footer>
+
+			<div class="inner">
+				
+				<p>All Rights Reserved | <?php echo date("Y"); ?> | tell-tale MEDIA</p>
+
+			</div><!--inner-->
+
+		</footer>
+
+
+	</body>
+</html>
---
+++ _assets/inc/navigation.php
@@ -0,0 +1,13 @@
+</head>
+<body>
+	<header class="main">
+		<nav class="main-nav">
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			<ul>
+
+	</header> <!-- header -->
+
