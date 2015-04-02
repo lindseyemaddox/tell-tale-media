ddec76a3b006090e26eb5cb2ffedd7fcea30a43d
lindseyemaddox <lindseyemaddox@gmail.com>
2015-04-02 00:10:41 -0500

    tweaks, remove isotope

--- design.php
+++ design.php
@@ -6,16 +6,16 @@
 <script type="text/javascript">
   $(document).ready(function() {
     $('.fancybox').fancybox();
-			$(".fancybox-effects-a").fancybox({
-				helpers: {
-					title : {
-						type : 'outside'
-					},
-					overlay : {
-						speedOut : 0
-					}
-				}
-			});
+	$(".fancybox-effects-a").fancybox({
+		helpers: {
+			title : {
+				type : 'outside'
+			},
+			overlay : {
+				speedOut : 0
+			}
+		}
+	});
   });
 </script>
 <script src="/_assets/js/jquery.fancybox.js"></script>
@@ -36,20 +36,6 @@
 	<div class="inner">
 
 		<div class="container">
-
-		    <div id="options" class="clearfix">
-
-		        <ul id="filters" class="option-set clearfix" data-option-key="filter">
-		          <li><a href="#filter" data-option-value="*" class="selected">show all</a></li>
-		          <li><a href="#filter" data-option-value=".websites">websites</a></li>
-		          <li><a href="#filter" data-option-value=".posters">posters</a></li>
-		          <li><a href="#filter" data-option-value=".branding">branding</a></li>
-		          <li><a href="#filter" data-option-value=".brochures">brochures</a></li>
-		          <li><a href="#filter" data-option-value=".t-shirts">t-shirts</a></li>
-		          <li><a href="#filter" data-option-value=".packaging">packaging</a></li>
-		        </ul>
-
-		    </div> <!-- #options -->
 
 		    <div id="design-container" class="clearfix">
 
@@ -112,64 +98,5 @@
 	</div><!--inner-->
 
 </section><!--design-->
-
-<script>
-
-    $(function(){
-
-      var $container = $('#design-container');
-
-      $container.isotope({
-        itemSelector : '.piece'
-      });
-
-
-      var $optionSets = $('#options .option-set'),
-          $optionLinks = $optionSets.find('a');
-
-      $optionLinks.click(function(){
-        var $this = $(this);
-        // don't proceed if already selected
-        if ( $this.hasClass('selected') ) {
-          return false;
-        }
-        var $optionSet = $this.parents('.option-set');
-        $optionSet.find('.selected').removeClass('selected');
-        $this.addClass('selected');
-
-        // make option object dynamically, i.e. { filter: '.my-filter-class' }
-        var options = {},
-            key = $optionSet.attr('data-option-key'),
-            value = $this.attr('data-option-value');
-        // parse 'false' as false boolean
-        value = value === 'false' ? false : value;
-        options[ key ] = value;
-        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-          // changes in layout modes need extra logic
-          changeLayoutMode( $this, options )
-        } else {
-          // otherwise, apply new options
-          $container.isotope( options );
-        }
-
-        return false;
-      });
-
-      $( ".slider" ).slider({
-        value:0,
-        min: 0,
-        max: 15,
-        step: 1,
-        slide: function( event, ui ) {
-          var slideVal = ui.value;
-          $('#filters li:eq('+ slideVal +') a').trigger('click');
-          $('#filters li:eq('+ slideVal +') a').css('color','blue');
-        }
-      });
-
-
-    });
-
-</script>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
