ff6177102a4105bfa082b3b870c678fcb01e5988
Lindsey Maddox <dev@lindseymaddox.com>
2015-12-07 15:03:31 -0600

    portfolio update, responsive changes on design page

--- design.php
+++ design.php
@@ -40,52 +40,309 @@
 		    <div id="design-container" class="clearfix">
 
 		      	<article class="piece websites">
-		      		<a href="/_assets/img/portfolio-website-hudson.jpg" class="fancybox" rel="websites" title="Web design for the Bill Hudson Agency"><img src="/_assets/img/thumb-website-hudson.jpg" alt="web design for the Bill Hudson Agency"></a>
+
+		      		<img src="/_assets/img/thumb-hudson-agency.jpg" alt="web design for the Bill Hudson Agency">
+
+		      		<div class="hover">
+		      			<h3>Freelance website design project for the Bill Hudson Agency. Established in 1963, the Bill Hudson Agency (BHA) is the oldest and one of the largest marketing and communications firms in Nashville. Their motto is “Do the right thing and do it well” and their mascot is Addy, the majestic Goldendoodle.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-hudson-index.jpg" class="btn two fancybox" rel="hudson" title="Web design for the Bill Hudson Agency">View Slides</a>
+			      			<a class="btn two" href="http://www.billhudsonagency.com/" target="_blank" title="Bill Hudson Agency Website">Visit Site</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+					<div class="hidden">
+					  <a class="fancybox" rel="hudson" title="Web design for the Bill Hudson Agency" href="/_assets/img/portfolio-hudson-about.jpg"><img src="/_assets/img/portfolio-hudson-about.jpg" alt="Bill Hudson Agency about page"/></a>
+					  <a class="fancybox" rel="hudson" title="Web design for the Bill Hudson Agency" href="/_assets/img/portfolio-hudson-work.jpg"><img src="/_assets/img/portfolio-hudson-work.jpg" alt="Bill Hudson Agency work page"/></a>
+					  <a class="fancybox" rel="hudson" title="Web design for the Bill Hudson Agency" href="/_assets/img/portfolio-hudson-folks.jpg"><img src="/_assets/img/portfolio-hudson-folks.jpg" alt="Bill Hudson Agency folks page"/></a>
+					  <a class="fancybox" rel="hudson" title="Web design for the Bill Hudson Agency" href="/_assets/img/portfolio-hudson-contact.jpg"><img src="/_assets/img/portfolio-hudson-contact.jpg" alt="Bill Hudson Agency contact page"/></a>
+					</div><!--hidden-->
+
+		      	</article>
+
+		      	<article class="piece branding">
+
+		      		<img src="/_assets/img/thumb-ronald-mcdonald-house.jpg" alt="Advertisement for the Ronald McDonald House">
+
+		      		<div class="hover">
+		      			<h3>“Strength In Numbers” ad for the Ronald McDonald House’s Give a Penny campaign. The Ronald McDonald house finds, creates and supports programs that directly improve the health and well being of sick children while providing comfort and care to children and their families. Freelance design project for the Bill Hudson Agency.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-ronald-mcdonald.jpg" class="btn fancybox" title="Advertisement for the Ronald McDonald House">View Ad</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece brochures">
+
+		      		<img src="/_assets/img/thumb-brochure-nutcracker.jpg" alt="Mini-Nutcracker brochure">
+
+		      		<div class="hover">
+		      			<h3>The Mini-Nutcracker is a long time family tradition wrapped in the spirit of Christmas. Designed to introduce children to ballet as performance art, this magical show is performed by The Centennial Youth Ballet. Freelance brochure design for the Bill Hudson Agency. </h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-brochure-nutcracker.jpg" class="btn fancybox" title="Mini-Nutcracker brochure">View Brochure</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece branding">
+		      		
+		      		<img src="/_assets/img/thumb-watershed.jpg" alt="Restoring Our Watershed logo">
+
+		      		<div class="hover">
+		      			<h3>Restoring Our Watershed is a US-based non-profit working in the Nandamojo River Valley of Guanacaste, Costa Rica. They are committed to restoring and protecting the land, water, flora and fauna of the Nandamojo watershed for future generations.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-watershed.jpg" class="btn fancybox" title="Restoring Our Watershed logo">View Logo</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece websites">
+		      		
+		      		<img src="/_assets/img/thumb-website-rosepepper.jpg" alt="Rosepepper Cantina website design">
+
+		      		<div class="hover">
+		      			<h3>Website design for Rosepepper Cantina, one of East Nashville's oldest go-to spots and winner of numerous awards including Nashville's Best Mexican Restaurant and Best Margarita, for the 11th year in a row. Tequila!</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-website-rosepepper.jpg" class="btn fancybox" title="Web design for Rosepepper Cantina">View Design</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
 		      	<article class="piece posters">
-		      		<a href="/_assets/img/portfolio-poster-duty.jpg" class="fancybox" title="Duty of the Hour poster"><img src="/_assets/img/thumb-poster-duty.jpg" alt="Duty of the Hour poster"></a>
-		      	</article>
 
-		      	<article class="piece branding">
-		      		<a href="/_assets/img/portfolio-branding-vintage.jpg" class="fancybox" title="Village Vintage and Homegoods logo"><img src="/_assets/img/thumb-branding-vintage.jpg" alt="Village Vintage and Homegoods logo"></a>
-		      	</article>
+		      		<img src="/_assets/img/thumb-poster-duty.jpg" alt="Duty of the Hour poster">
 
-		      	<article class="piece brochures">
-		      		<a href="/_assets/img/portfolio-brochure-nutcracker.jpg" class="fancybox" title="Mini-Nutcracker brochure"><img src="/_assets/img/thumb-brochure-nutcracker.jpg" alt="Mini-Nutcracker brochure"></a>
-		      	</article>
+		      		<div class="hover">
+		      			<h3>Poster Design for our documentary Duty of the Hour, commissioned by the Benjamin L. Hooks Institute For Social Change at the University of Memphis. Screened at the 2015 Nashville Film Festival and shown on numerous outlets including PBS. </h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-poster-duty.jpg" class="btn fancybox" title="Duty of the Hour poster">View Poster</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
 
-		      	<article class="piece brochures">
-		      		<a href="/_assets/img/portfolio-brochure-push.jpg" class="fancybox" title="Push Childbirth Education brochure"><img src="/_assets/img/thumb-brochure-push.jpg" alt="Push Childbirth Education brochure"></a>
 		      	</article>
 
 		      	<article class="piece websites">
-		      		<a href="/_assets/img/portfolio-website-rosepepper.jpg" class="fancybox" title="Rosepepper Cantina website"><img src="/_assets/img/thumb-website-rosepepper.jpg" alt="Rosepepper Cantina website"></a>
+		      		
+		      		<img src="/_assets/img/thumb-website-ceramics.jpg" alt="Mid-South Ceramics website design">
+
+		      		<div class="hover">
+		      			<h3>Website design for The Clay Lady Studio and Mid-South Ceramics. The Clay Lady campus is home to 65 co-op artists, a 3,000 square foot studio and Mid-South Ceramics art supply warehouse. They offer classes for children and adults and host many art shows and workshops annually.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-website-ceramics.jpg" class="btn fancybox" title="The Clay Lady Studio website design">View Design</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece brochures">
+
+		      		<img src="/_assets/img/thumb-brochure-push.jpg" alt="Push Childbirth Education brochure">
+
+		      		<div class="hover">
+		      			<h3>Push Childbirth Education brochure design. “Inspiring Confidence in Expectant Parents,” Push offers childbirth education designed to help both partners understand the process of pregnancy and childbirth in comfortable, safe setting.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-brochure-push.jpg" class="btn fancybox" title="Push Childbirth Education brochure">View Brochure</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
 		      	<article class="piece branding">
-		      		<a href="/_assets/img/portfolio-logo-pure.jpg" class="fancybox" title="Pure Body Revolution logo"><img src="/_assets/img/thumb-logo-pure.jpg" alt="Pure Body Revolution logo"></a>
+		      		
+		      		<img src="/_assets/img/thumb-logo-hands.jpg" alt="Hands 4 NY logo">
+
+		      		<div class="hover">
+		      			<h3>Logo design for Hands 4 NY, a volunteer program dedicated to collecting and distributing food to city residents in need. Designed for United Methodist Communications. </h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-logo-hands.jpg" class="btn fancybox" title="Hands 4 NY logo">View Logo</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
-		      	<article class="piece websites">
-		      		<a href="/_assets/img/portfolio-website-ceramics.jpg" class="fancybox" title="Mid-South Ceramics website"><img src="/_assets/img/thumb-website-ceramics.jpg" alt="Mid-South Ceramics website"></a>
+		      	<article class="piece brochures">
+
+		      		<img src="/_assets/img/thumb-tech-shop.jpg" alt="Tech Shop brochure design">
+
+		      		<div class="hover">
+		      			<h3>Tech Shop provides technology and software for ministries in the United Methodist Church. They also provide special tools for volunteers in disaster zones and remote regions.  Brochure Design for United Methodist Communications.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-tech-shop.jpg" class="btn fancybox" title="Tech Shop brochure design">View Brochure</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
-		      	<article class="piece t-shirts">
-		      		<a href="/_assets/img/portfolio-shirt-travis.jpg" class="fancybox" title="Travis Meadows t-shirt"><img src="/_assets/img/thumb-shirt-travis.jpg" alt="Travis Meadows t-shirt"></a>
+		      	<article class="piece branding">
+
+		      		<img src="/_assets/img/thumb-outside-the-box.jpg" alt="Advertisement for United Methodist Communications">
+
+		      		<div class="hover">
+		      			<h3>Outside the Box encourages people to think about making their messages come alive in unexpected ways. Ad design for United Methodist Communications.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-outside-the-box.jpg" class="btn fancybox" title="Advertisement for United Methodist Communications">View Ad</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece branding">
+
+		      		<img src="/_assets/img/thumb-calendar.jpg" alt="Calendar advertisement for United Methodist Communications">
+
+		      		<div class="hover">
+		      			<h3>2016 calendar ad providing a variety of calendars and planners for the upcoming year. Ad created for United Methodist Communications.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-calendar.jpg" class="btn fancybox" title="Calendar advertisement for United Methodist Communications">View Ad</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
 		      	<article class="piece packaging">
-		      		<a href="/_assets/img/portfolio-cd-travis.jpg" class="fancybox" title="Travis Meadows cd"><img src="/_assets/img/thumb-cd-travis.jpg" alt="Travis Meadows cd"></a>
+
+		      		<img src="/_assets/img/thumb-cd-travis.jpg" alt="Travis Meadows CD design">
+
+		      		<div class="hover">
+		      			<h3>Live at Natchez Hills Vineyard CD packaging design for artists Travis Meadows and Levi Lowrey.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-cd-travis.jpg" class="btn fancybox" title="Travis Meadows CD design">View CD</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece t-shirts">
+
+		      		<img src="/_assets/img/thumb-shirt-travis.jpg" alt="Travis Meadows t-shirt design">
+
+		      		<div class="hover">
+		      			<h3>Travis Meadows t-Shirt design. Travis has written songs for Dierks Bentley, Eric Church, Wynonna Judd, Lynyrd Skynyrd and Blackberry Smoke. He is also a successful artist and was this year’s keynote speaker at the NSAI songwriter conference.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-shirt-travis.jpg" class="btn fancybox" title="Travis Meadows t-shirt design">View T-Shirt</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
 		      	</article>
 
 		      	<article class="piece posters">
-		      		<a href="/_assets/img/portfolio-poster-country.jpg" class="fancybox" title="Good Country People Party poster"><img src="/_assets/img/thumb-poster-country.jpg" alt="Good Country People Party poster"></a>
+
+		      		<img src="/_assets/img/thumb-travis-deer.jpg" alt="Travis Meadows poster design">
+
+		      		<div class="hover">
+		      			<h3>Poster Design for Travis Meadows, who Rolling Stone called “Nashville’s most bad-ass songwriter.”</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-travis-poster.jpg" class="btn fancybox" title="Travis Meadows poster design">View Poster</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece posters">
+
+		      		<img src="/_assets/img/thumb-poster-country.jpg" alt="Good Country People Party poster">
+
+		      		<div class="hover">
+		      			<h3>Poster designs for the Good Country People Party featuring Travis Meadows and Levi Lowrey.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-poster-country.jpg" rel="country" class="btn fancybox" title="Good Country People Party poster">View Posters</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+					<div class="hidden">
+					  <a class="fancybox" rel="country" title="Good Country People Party poster" href="/_assets/img/portfolio-poster-country-2.jpg"><img src="/_assets/img/portfolio-poster-country-2.jpg" alt="Good Country People Party poster"/></a>
+					</div><!--hidden-->
+
 		      	</article>
 
 		      	<article class="piece branding">
-		      		<a href="/_assets/img/portfolio-logo-hands.jpg" class="fancybox" title="Hands 4 NY logo"><img src="/_assets/img/thumb-logo-hands.jpg" alt="Hands 4 NY logo"></a>
+
+		      		<img src="/_assets/img/thumb-omnibus.jpg" alt="Ad designs for the Omnibus Center">
+
+		      		<div class="hover">
+		      			<h3>Ad designs for the Omnibus Center, which offers meaningful courses such as Blueprint for Resilience and the famed Hero’s Journey.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-omnibus-journey.jpg" rel="omnibus" class="btn fancybox" title="Ad designs for the Omnibus Center">View Ads</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+					<div class="hidden">
+					  <a class="fancybox" rel="omnibus" title="Ad designs for the Omnibus Center" href="/_assets/img/portfolio-omnibus-coaching.jpg"><img src="/_assets/img/portfolio-omnibus-coaching.jpg" alt="Ad designs for the Omnibus Center"/></a>
+					</div><!--hidden-->
+
 		      	</article>
+
+		      	<article class="piece branding">
+		      		
+		      		<img src="/_assets/img/thumb-logo-pure.jpg" alt="Pure Body Revolution logo">
+
+		      		<div class="hover">
+		      			<h3>Logo design for Pure Body Revolution, a Tabata Boot Camp guaranteed to change the way you think about exercise and nutrition and help you be the best possible version of yourself.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-logo-pure.jpg" class="btn fancybox" title="Pure Body Revolution logo">View Logo</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece branding">
+		      		
+		      		<img src="/_assets/img/thumb-branding-vintage.jpg" alt="Village Vintage and Homegoods logo">
+
+		      		<div class="hover">
+		      			<h3>Logo design of Village Vintage and Homegoods, a popular boutique located in beautiful downtown Leipers Fork, Tennessee.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-branding-vintage.jpg" class="btn fancybox" title="Village Vintage and Homegoods logo">View Logo</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece brochures">
+
+		      		<img src="/_assets/img/thumb-history.jpg" alt="History Houses logo and brochure design">
+
+		      		<div class="hover">
+		      			<h3>History Houses logo and brochure design. Located in Leipers Fork, TN, History Houses builds new homes using antique fixtures and elements. Their slogan is “We build new houses with old souls.”</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-history.jpg" class="btn fancybox" title="History Houses logo and brochure design">View Brochure</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece branding">
+		      		
+		      		<img src="/_assets/img/thumb-sweet-thyme.jpg" alt="Sweet Thyme Farm logo">
+
+		      		<div class="hover">
+		      			<h3>Logo design for Sweet Thyme Farm, which produces delicious local honey in Franklin, TN.</h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-sweet-thyme.jpg" class="btn fancybox" title="Sweet Thyme Farm logo">View Logo</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+		      	<article class="piece posters">
+
+		      		<img src="/_assets/img/thumb-family-wash.jpg" alt="Family Wash poster design">
+
+		      		<div class="hover">
+		      			<h3>Radio Free Nashville poster design for a live broadcast featuring the Blackfoot Gypsies and Etta Britt at the famed East Nashville bar The Family Wash. </h3>
+		      			<div class="btns">
+			      			<a href="/_assets/img/portfolio-family-wash.jpg" class="btn fancybox" title="Radio Free Nashville poster design">View Poster</a>
+						</div><!--btns-->
+		      		</div><!--hover-->
+
+		      	</article>
+
+
+
 
 		      	<article class="piece websites posters branding brochures t-shirts packaging">
 		      		<img src="/_assets/img/thumb-your-work.jpg" alt="your work here" title="your work here">
